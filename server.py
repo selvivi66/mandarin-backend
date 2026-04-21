3b88942d30e3f501189951811b53186ed49fef2f
selvivi66 <selvi.sanjaya@gmail.com>
2026-04-21 20:13:54 +0700

    fix indentation

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
+from flask import Flask
+
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API is running"
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
+    return {"status": "ok"}   # 👈 THIS LINE FIXES YOUR ERROR
